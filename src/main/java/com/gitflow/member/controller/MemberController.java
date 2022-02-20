50d1d1a0950753a0cf8942c78eaa4631fcfd3dce
ym1085 <youngmin1085@gmail.com>
2022-02-20 15:25:20 +0900

    FEAT: 코드 리뷰 후 로그인 기능 재 업로드
    1. 이상한 로그인 파일이 업로드 되어 수정 후 재 업로드

--- src/main/java/com/gitflow/member/controller/MemberController.java
+++ src/main/java/com/gitflow/member/controller/MemberController.java
@@ -1,2 +1,20 @@
-package com.gitflow.member.controller;public class MemberController {
+package com.gitflow.member.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MemberController {
+
+    private final Logger log = LoggerFactory.getLogger(MemberController.class);
+
+    @GetMapping("/user")
+    public ResponseEntity<String> login() {
+        return ResponseEntity.ok().body("로그인 완료");
+    }
 }
